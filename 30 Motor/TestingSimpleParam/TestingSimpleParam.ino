ce3bd6764f98cbc196dd0709e22964d5d0467d17
Mark Mercer <Mark.Mercer@epsb.ca>
2025-04-15 13:38:35 -0600

    Update TestingSimpleParam.ino

--- 30 Motor/TestingSimpleParam/TestingSimpleParam.ino
+++ 30 Motor/TestingSimpleParam/TestingSimpleParam.ino
@@ -2,16 +2,17 @@ const int motorPin = 9;  // Define the pin connected to the motor's speed contro
 int motorParameter = 82;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);  // initialize digital pin LED_BUILTIN as an output pin.
   // Set the motor pin as an output
   pinMode(motorPin, OUTPUT);
-  println("End Setup, Motor Parameter is", motorParameter);
+  Serial.println("Motor", motorParameter);
 }
 
 void loop() {
   on(1000, 500); //Verifies Code executes
   analogWrite(motorPin, -motorParameter); // Set the motor speed to xxx (max 255)
-  println("Motor Parameter", motorParameter);
+  Serial.println("Motor", motorParameter);
   delay(1000);  // Wait for 1 second
   motorParameter++;
 }
