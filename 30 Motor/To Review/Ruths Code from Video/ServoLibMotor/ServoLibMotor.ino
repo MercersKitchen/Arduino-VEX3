fbab605e1cd2b7c9fbcb5ff939034c6601b6bb5b
Mark Mercer <Mark.Mercer@epsb.ca>
2025-04-25 13:39:00 -0600

    Update ServoLibMotor.ino

--- 30 Motor/To Review/Ruths Code from Video/ServoLibMotor/ServoLibMotor.ino
+++ 30 Motor/To Review/Ruths Code from Video/ServoLibMotor/ServoLibMotor.ino
@@ -2,34 +2,25 @@
 // See: https://docs.arduino.cc/libraries/servo/
 //
 int leftMotorPin = 4;   //left motor pin
-int rightMotorPin = 2;  //left motor pin
 Servo leftMotor;
-Servo rightMotor;
 //
 void setup() {
   // put your setup code here, to run once:
   pinMode(leftMotorPin, OUTPUT);
-  pinMode(rightMotorPin, OUTPUT);
   pinMode(13, OUTPUT);
   leftMotor.attach(leftMotorPin);
-  rightMotor.attach(rightMotorPin);
 }
 //
 void loop() {
-  // put your main code here, to run repeatedly
-  leftMotorControl(100);
-  rightMotorControl(600);
+  //0-1023, 500 center for opposite direction (no movement), further away is more movement
+  leftMotorControl(1000); 
+  //
   digitalWrite(13, HIGH);
-  delay(1000);
-  leftMotorControl(-100);
-  rightMotorControl(-100);
-  digitalWrite(13, LOW);
+  delay(3000);
 }
 //
 int leftMotorControl(int value) {
-  leftMotor.write(map(value, -100, 100, 1000, 2000));
+  leftMotor.write(map(value, 0, 1023, 1000, 2000));
 }
 //
-int rightMotorControl(int value) {
-  rightMotor.write(map(value, -100, 100, 1000, 2000));
-}
+//End MAIN
